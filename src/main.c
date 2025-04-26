1e54beb1bed7b9ab27c1df9dd529fd64fd0392c7
Yago Guirra <yagoguirra@hotmail.com>
2025-04-26 13:47:12 -0300

    fiz a logica para printar as cores, falta apenas resolver o problema que as cores nos leds não estão ficando bem feitas

--- src/main.c
+++ src/main.c
@@ -1,5 +1,7 @@
 // lib padrão do c
 #include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
 
 // lib da pico
 #include "pico/stdlib.h"
@@ -18,7 +20,9 @@
 #define KNOWN_RESISTOR 10000
 #define E24_SIZE 24
 
-#define REAL_VALUES 0
+#define REAL_VALUES 1
+
+#define DEBUG(val) printf("%s: %d\n", #val, val)
 
 // variaves globais
 float r_unknown = 0.f;
@@ -34,9 +38,9 @@ const float E24_VALUES[] = {
 // cores padrão
 const rgb_led COLOR_TABLE[10] = {
     {0, 0, 0},         // 0 - Preto
-    {139, 69, 19},     // 1 - Marrom
+    {100, 20, 0},      // 1 - Marrom
     {255, 0, 0},       // 2 - Vermelho
-    {255, 165, 0},     // 3 - Laranja
+    {246, 20, 0},     // 3 - Laranja
     {255, 255, 0},     // 4 - Amarelo
     {0, 128, 0},       // 5 - Verde
     {0, 0, 255},       // 6 - Azul
@@ -45,17 +49,24 @@ const rgb_led COLOR_TABLE[10] = {
     {255, 255, 255}    // 9 - Branco
 };
 
+const char *COLOR_NAMES[10] = {
+    "Preto", "Marrom", "Vermelho", "Laranja",
+    "Amarelo", "Verde", "Azul", "Violeta",
+    "Cinza", "Branco"
+};
+
+int color_index[3];
+
 // função para inicialização dos componentes
 void setup();
 
 // função para desenhar a tela inicial
 void draw_screen(char *, char *, display *);
 
-// Função para encontrar o valor comercial E24 mais próximo
+// função para encontrar o valor comercial E24 mais próximo e a cor
 float find_nearest_e24(float resistance);
-
-// Função para obter as cores (em rgb_led) correspondentes ao valor
 void get_resistor_colors(float resistance, rgb_led colors[3]);
+
 
 int main() {
     stdio_init_all();
@@ -86,13 +97,28 @@ int main() {
 
         r_unknown = (KNOWN_RESISTOR * media) / (ADC_RESOLUTION - media);
 
-        sprintf(adc_string, "%1.0f", media);
-        sprintf(resistor_string, "%1.0f", r_unknown);
+        DEBUG((int)r_unknown);
 
+        float r_e24 = find_nearest_e24(r_unknown);
+        rgb_led colors[3];
+        get_resistor_colors(r_e24, colors);
+
+        sprintf(adc_string, "%1.0f", media);
+        sprintf(resistor_string, "%1.0f", r_e24);
+        
         draw_screen(adc_string, resistor_string, &dp);
 
+        matrix_clear();
+        matrix_set_led_xy(2, 4, colors[0]);
+        matrix_set_led_xy(2, 3, colors[1]);
+        matrix_set_led_xy(2, 2, colors[2]);
+        matrix_update();
         
-        sleep_ms(100);
+        printf("%.0f\n%d %d %d\n", r_e24, colors[0].r, colors[0].g, colors[0].b);
+        // printf("%d %d %d\n",colors[1].r, colors[1].g, colors[1].b);
+        // printf("%d %d %d\n",colors[2].r, colors[2].g, colors[2].b);
+
+        sleep_ms(1000);
     }
 }
 
@@ -110,10 +136,12 @@ void draw_screen(char *adc, char *resistor, display *dp) {
 
     display_draw_rectangle(3, 3, 122, 60, false, true, dp);
 
-    display_draw_line(3, 25, 123, 25, true, dp);
+    display_draw_string(37, 7, COLOR_NAMES[color_index[0]], true, dp);
+    display_draw_string(37, 17, COLOR_NAMES[color_index[1]], true, dp);
+    display_draw_string(37, 27, COLOR_NAMES[color_index[2]], true, dp);
+
     display_draw_line(3, 37, 123, 37, true, dp);
 
-    display_draw_string(10, 28, "Ohmimetro", true, dp);
     display_draw_string(13, 41, "ADC", true, dp);
     display_draw_string(50, 41, "Resisten.", true, dp);
 
@@ -123,4 +151,58 @@ void draw_screen(char *adc, char *resistor, display *dp) {
     display_draw_string(59, 52, resistor, true, dp);
 
     display_update(dp);
+}
+
+float find_nearest_e24(float resistance) {
+    float min_diff = 1e9;
+    float nearest = 0;
+
+    // Procura o valor E24 mais próximo em todas as escalas
+    for (int exp = -1; exp <= 6; exp++) { // De 0.1 a 10MΩ, pode ajustar se quiser
+        float factor = powf(10, exp);
+        for (int i = 0; i < 24; i++) {
+            float candidate = E24_VALUES[i] * factor;
+            float diff = fabsf(resistance - candidate);
+            if (diff < min_diff) {
+                min_diff = diff;
+                nearest = candidate;
+            }
+        }
+    }
+    // DEBUG(nearest);
+    return nearest;
+}
+
+
+// Gera as três cores correspondentes
+void get_resistor_colors(float resistance, rgb_led colors[3]) {
+    int first_digit, second_digit, multiplier = 0;
+
+    // Normaliza para dois dígitos
+    while (resistance >= 100) {
+        resistance /= 10;
+        multiplier++;
+    }
+    while (resistance < 10) {
+        resistance *= 10;
+        multiplier--;
+    }
+
+    int value = (int)(resistance + 0.5); // Arredonda
+
+    first_digit = value / 10;
+    second_digit = value % 10;
+
+    // Atribui as cores
+    // DEBUG(first_digit);
+    // DEBUG(second_digit);
+    // DEBUG(multiplier);
+
+    color_index[0] = first_digit;
+    color_index[1] = second_digit;
+    color_index[2] = multiplier;
+
+    colors[0] = COLOR_TABLE[first_digit];
+    colors[1] = COLOR_TABLE[second_digit];
+    colors[2] = COLOR_TABLE[multiplier];
 }
